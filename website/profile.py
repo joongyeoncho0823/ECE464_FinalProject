f9b32ba0bca98fc72f02c8d8e7621f3178fbdc9f
Joongyeon Cho <joongyeoncho0823@gmail.com>
2021-12-10 15:00:24 -0500

    commit

--- website/profile.py
+++ website/profile.py
@@ -8,13 +8,15 @@ profile = Blueprint('profile', __name__)
 
 
 @profile.route('/profile/<int:user_id>')
-def viewOtherProfile(user_id):
+def viewProfile(user_id):
     user = User.query.filter_by(id=user_id).first()
-    if user.id == current_user.id:
-        return redirect(url_for('profile.viewProfile'))
     return render_template("profile.html", user=user)
 
 
-@profile.route('/profile')
-def viewProfile():
-    return render_template("myprofile.html", user=current_user)
+# @profile.route('/profile')
+# def viewProfile():
+#     return render_template("profile.html", user=current_user)
+
+@profile.route('/updateProfile')
+def updateProfile():
+    return render_template("updateInfo.html", user=current_user)
